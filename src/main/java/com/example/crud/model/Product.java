54b2063e921da0beaeb283b73e04d40a21d2a2f7
RubensZaes <rubenszaes@gmail.com>
2023-07-26 08:46:33 -0300

    Entity Product

--- src/main/java/com/example/crud/model/Product.java
+++ src/main/java/com/example/crud/model/Product.java
@@ -1,2 +1,27 @@
-package com.example.crud.model;public class Product {
+package com.example.crud.model;
+
+import com.example.crud.dtos.RequestProductDTO;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity(name = "product")
+@Table(name = "product")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Integer price_in_cents;
+
+    public Product(RequestProductDTO requestProductDTO) {
+        this.name = requestProductDTO.name();
+        this.price_in_cents = requestProductDTO.price_in_cents();
+    }
 }
