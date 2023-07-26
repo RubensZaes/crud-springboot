65815249d5f222980f358b8d8d2b05c4ef802fcd
RubensZaes <rubenszaes@gmail.com>
2023-07-26 08:49:17 -0300

    DTO Request Product

--- src/main/java/com/example/crud/dtos/RequestProductDTO.java
+++ src/main/java/com/example/crud/dtos/RequestProductDTO.java
@@ -1,2 +1,17 @@
-package com.example.crud.dtos;public record RequestProductDTO() {
+package com.example.crud.dtos;
+
+import com.example.crud.model.Product;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record RequestProductDTO(
+        Long id,
+        @NotBlank
+        String name,
+        @NotNull
+        Integer price_in_cents
+) {
+        public RequestProductDTO(Product product) {
+                this(product.getId(), product.getName(), product.getPrice_in_cents());
+        }
 }
